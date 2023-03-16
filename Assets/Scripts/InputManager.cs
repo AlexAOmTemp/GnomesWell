1c1f4fb0ee091f97dd000a1714b08d76559ddaf7
Alex.A.Om.temp <Alex.A.Om.temp@gmail.com>
2023-03-16 12:08:56 +0400

    init commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,35 @@
+﻿// BEGIN 2d_inputmanager
+
+using UnityEngine;
+using System.Collections;
+
+// Translates the accelerometer data into sideways motion info.
+public class InputManager : Singleton<InputManager> {
+
+	// How much we're moving. -1.0 = full left, +1.0 = full right
+	private float _sidewaysMotion = 0.0f;
+
+    // This property is declared as read-only, so that other classes can't 
+    // change it
+    public float sidewaysMotion {
+		get {
+			return _sidewaysMotion;
+		}
+	}
+	
+	// Every frame, store the tilt
+	void Update () {
+#if UNITY_ANDROID
+		Vector3 accel = Input.acceleration;
+		_sidewaysMotion = accel.x;
+#else
+        _sidewaysMotion = Input.GetAxis("Horizontal");
+#endif
+    }
+    void OnGUI()
+    {
+		//GUI.Label(new Rect(0, 0, 1000, 100), "position: " + transform.position.ToString());
+		GUI.Label(new Rect(0, 40, 1000, 100), "_sidewaysMotion " + _sidewaysMotion.ToString());
+    }
+}
+// END 2d_inputmanager
---
+++ Assets/Scripts/Swinging.cs
@@ -0,0 +1,40 @@
+﻿// BEGIN 2d_swinging
+
+using UnityEngine;
+using System.Collections;
+
+// Uses the input manager to apply sideways forces to an object.
+// Used to make the gnome swing side-to-side.
+public class Swinging : MonoBehaviour {
+
+	// How much should we swing by? Bigger numbers = more swing
+	public float swingSensitivity = 0.5f;
+
+	// Use FixedUpdate instead of Update, in order to play better with 
+	// the physics engine
+	private Vector2 _force;
+
+    void FixedUpdate() {
+
+		// If we have no ridigbody (anymore), remove this component
+		if (GetComponent<Rigidbody2D>() == null) {
+			Destroy (this);
+			return;
+		}
+
+		// Get the tilt amount from the InputManager
+		float swing = InputManager.instance.sidewaysMotion;
+
+		// Calculate a force to apply
+		_force = new Vector2(swing * swingSensitivity, 0);
+		
+		// Apply the force
+		GetComponent<Rigidbody2D>().AddForce(_force);
+	}
+    void OnGUI()
+    {
+        //GUI.Label(new Rect(0, 0, 1000, 100), "position: " + transform.position.ToString());
+        GUI.Label(new Rect(0, 30, 1000, 100), "force " + _force.ToString());
+    }
+}
+// END 2d_swinging
